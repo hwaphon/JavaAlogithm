d224dc731bedcc1d89b7378a8f9bccf4438dd61d
hwaphon <969489625@qq.com>
2017-03-15 19:48:00 +0800

    自定义栈

---
+++ chapter3/Stack.java
@@ -0,0 +1,75 @@
+package com.chapter3;
+
+public class Stack {
+
+	private Node top = null;
+
+	private class Node {
+		int data;
+		Node next = null;
+
+		public Node(int data) {
+			this.data = data;
+		}
+	}
+
+	public void push(int d) {
+		Node n = new Node(d);
+
+		n.next = top;
+		top = n;
+	}
+
+	public int peek() {
+		return top.data;
+	}
+
+	public Node pop() {
+
+		Node item = null;
+
+		if (top != null) {
+			item = top;
+			top = top.next;
+		}
+
+		return item;
+	}
+
+	public Node delete(int d) {
+
+		Node temp = null;
+
+		if (top != null) {
+
+			if (top.data == d) {
+				top = top.next;
+			} else {
+
+				Node current = top;
+
+				while (current.next != null) {
+					if (current.next.data == d) {
+						temp = current.next;
+						current.next = current.next.next;
+						break;
+					}
+
+					current = current.next;
+				}
+			}
+		}
+
+		return temp;
+	}
+
+	public void print() {
+		Node current = top;
+
+		while (current != null) {
+			System.out.println(current.data);
+			current = current.next;
+		}
+	}
+
+}
